4891e89a2d868516cbae8ef88e96bf3d64e986fc
lix3972 <35994518+lix3972@users.noreply.github.com>
2018-11-02 05:04:11 +0800

    Add files via upload

---
+++ cycle.py
@@ -0,0 +1,19 @@
+
+import matplotlib.pyplot as plt
+glassesX=[]
+glassesY=[]
+glassesXY=[]
+y=0
+for x in range(11):
+    glassesXY.append([x,y])
+for x in range(20,-1,-1):
+    y=int((100-(x-10)**2)**0.5)
+    glassesXY.append([x-10, y])
+for x in range(21):
+    y=-int((100-(x-10)**2)**0.5)
+    glassesXY.append([x-10, y])
+for x,y in glassesXY:
+    glassesX.append(x)
+    glassesY.append(y)
+plt.plot(glassesX,glassesY)
+plt.show()
