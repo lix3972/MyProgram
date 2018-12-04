21853c8d8ce4570e8805f88f8c4e4c55da1dd254
lix3972 <35994518+lix3972@users.noreply.github.com>
2018-12-04 09:56:55 +0800

    Add files via upload

--- myDiscoGan/DiscoGanImageTest.py
+++ myDiscoGan/DiscoGanImageTest.py
@@ -4,13 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from itertools import chain
 from data_process import *
-# import torch.autograd as Variable
+import tensorflow as tf
 
-BATCH_SIZE = 10
-LR_G = 0.0001  # learning rate for generator
-LR_D = 0.0001  # learning rate for discriminator
-
-# torch.cuda.set_device(0)
 G_A = nn.Sequential(  # Generator
     nn.Conv2d(3, 64, 4, 2, 1, bias=False),
     nn.LeakyReLU(0.2, inplace=True),
@@ -93,78 +88,98 @@ D_B = nn.Sequential(  # Discriminator
     nn.Sigmoid(),  # tell the probability that the art work is made by artist
 )
 
-G_A = G_A.cuda()
-G_B = G_B.cuda()
-D_A = D_A.cuda()
-D_B = D_B.cuda()
-G_params = chain(G_A.parameters(), G_B.parameters())
-D_params = chain(D_A.parameters(), D_B.parameters())
-opt_D = torch.optim.Adam(D_params, lr=LR_D)
-opt_G = torch.optim.Adam(G_params, lr=LR_G)
+# G_A = G_A.cuda()
+# G_B = G_B.cuda()
+# D_A = D_A.cuda()
+# D_B = D_B.cuda()
 
-recon_criterion = nn.MSELoss()
-gan_criterion = nn.BCELoss()
-feat_criterion = nn.HingeEmbeddingLoss()
-
-filePathA = 'dataset/train_im'
-filePathB = 'dataset/train_npy'
-classA=get_filenames(filePathA)
-classB=get_npy_filenames(filePathB)
-
-classA=read_images(classA,image_size=64)
-classB=read_npy(classB,image_size=64)
-
-for step in range(100000):
-
-    for i in range(BATCH_SIZE):
-        A0 = torch.Tensor(classA[i]) #artist_works_A()  # real painting from artist
-        A = A0.unsqueeze(0)
-
-        A = A.cuda()
-        B0 = torch.Tensor(classB[i]) #artist_works_B()
-        B = B0.unsqueeze(0)
-
-        B = B.cuda()
-
-        AB = G_B(A)
-        BA = G_A(B)
-
-        ABA = G_A(AB)
-        BAB = G_B(BA)
-
-        # gen first
-        recon_loss_A = recon_criterion(ABA, A)  # mse
-        L_GAB = -torch.log(D_B(G_B(A)))  # AB=G_B(A)
-        # gen second
-        recon_loss_B = recon_criterion(BAB, B)
-        L_GBA = -torch.log(D_A(G_A(B)))  # BA=G_A(B)
-        # gen total A B
-        G_loss = (recon_loss_A + L_GAB + recon_loss_B + L_GBA).cuda()
-
-        # dis
-        L_DB = -torch.log(D_B(B)) - torch.log(1. - D_B(AB))  # AB=G_B(A)
-        L_DA = -torch.log(D_A(A)) - torch.log(1. - D_A(BA))  # BA=G_A(B)
-        D_loss = (L_DB + L_DA).cuda()
-
-        opt_D.zero_grad()
-        D_loss.backward(retain_graph=True)  # reusing computational graph
-        opt_D.step()
-
-        opt_G.zero_grad()
-        G_loss.backward()
-        opt_G.step()
-
-
-
-        if i == BATCH_SIZE - 1:  # plotting
-            print('G_loss=',G_loss)
-            print('D_loss=',D_loss)
 G_A_path='pklFiles/G_A.pkl'
 G_B_path='pklFiles/G_B.pkl'
 D_A_path='pklFiles/D_A.pkl'
 D_B_path='pklFiles/D_B.pkl'
-torch.save(G_A.state_dict(), G_A_path)
-torch.save(G_B.state_dict(), G_B_path)
-torch.save(D_A.state_dict(), D_A_path)
-torch.save(D_B.state_dict(), D_B_path)
+
+# G_A = G_A()
+G_A.load_state_dict(torch.load(G_A_path))
+G_A.eval()
+
+G_B.load_state_dict(torch.load(G_B_path))
+G_B.eval()
+
+# D_A.load_state_dict(torch.load(D_A_path))
+# D_A.eval()
+#
+# D_B.load_state_dict(torch.load(D_B_path))
+# D_B.eval()
+
+filePathA = 'dataset/test_im'
+filePathB = 'dataset/test_npy'
+classA=get_filenames(filePathA)
+classB=get_npy_filenames(filePathB)
+classA=read_images(classA,image_size=720)
+classB=read_npy(classB,image_size=720)
+
+# plt.ion()   # something about continuous plotting
+
+for i in range(10):
+    A0 = torch.Tensor(classA[i])  # artist_works_A()  # real painting from artist
+    A = A0.unsqueeze(0)
+    # A = A.cuda()
+
+    B0 = torch.Tensor(classB[i])  # artist_works_B()
+    B = B0.unsqueeze(0)
+    # B = B.cuda()
+
+    AB = G_B(A)
+    BA = G_A(B)
+    ABA = G_A(AB)
+    BAB = G_B(BA)
+
+    plt.subplot(231)
+    plt.cla()
+    A = A.squeeze(0)
+    # A=A.numpy()
+    A = A.transpose(1,0)
+    A = A.transpose(1,2)
+    plt.imshow(A,label='A')
+
+    plt.subplot(232)
+    plt.cla()
+    AB=AB.squeeze(0)
+    AB = AB.transpose(1, 0)
+    AB = AB.transpose(1, 2)
+    AB = AB.data.cpu().numpy()
+    plt.imshow(AB[:,:,0])
+
+    plt.subplot(233)
+    plt.cla()
+    ABA = ABA.squeeze(0)
+    ABA = ABA.transpose(1, 0)
+    ABA = ABA.transpose(1, 2)
+    ABA = ABA.data.cpu().numpy()
+    plt.imshow(ABA)
+
+    plt.subplot(234)
+    plt.cla()
+    B = B.squeeze(0)
+    B = B.transpose(1, 0)
+    B = B.transpose(1, 2)
+    plt.imshow(B[:,:,0], label='B' )
+
+    plt.subplot(235)
+    plt.cla()
+    BA = BA.squeeze(0)
+    BA = BA.transpose(1, 0)
+    BA = BA.transpose(1, 2)
+    BA = BA.data.cpu().numpy()
+    plt.imshow(BA , label='BA' )
+
+    plt.subplot(236)
+    plt.cla()
+    BAB = BAB.squeeze(0)
+    BAB = BAB.transpose(1, 0)
+    BAB = BAB.transpose(1, 2)
+    BAB = BAB.data.cpu().numpy()
+    plt.imshow( BAB[:,:,0], label='BAB', )
+
+    plt.pause(0.5)
 
